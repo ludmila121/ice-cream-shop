72bb8b4dbc2cc242f7d62d644ae44377ca5a3bb4
Maiia Robeiko <maiia.robeiko@ukr.net>
2022-12-20 02:39:10 +0200

    Add animations and smal modalfix

--- src/js/slider.js
+++ src/js/slider.js
@@ -2,7 +2,14 @@ const swiper = new Swiper('.swiper', {
   // Optional parameters
   // direction: 'vertical',
   direction: 'horizontal',
-
+  autoplay: true,
+  // autoplayspeed: 6000,
+  autoplay: {
+    delay: 5000,
+  },
+  pauseOnFocus: true,
+  pauseOnHover: true,
+  pauseOnDotsHover: true,
   loop: true,
 
   // If we need pagination
