22e518d82e3b44bcce051d46defd2ef1ca1bff52
Sergii Podobrii <podobri@ukr.net>
2022-12-22 15:04:08 +0200

    update mobile-menu

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -2,6 +2,7 @@
   const mobileMenu = document.querySelector('.js-menu-container');
   const openMenuBtn = document.querySelector('.js-open-menu');
   const closeMenuBtn = document.querySelector('.js-close-menu');
+  const closeMenuLink = document.querySelectorAll('.mobile-menu__link');
 
   const toggleMenu = () => {
     const isMenuOpen = openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
@@ -11,7 +12,7 @@
     const scrollLockMethod = !isMenuOpen ? 'disableBodyScroll' : 'enableBodyScroll';
     bodyScrollLock[scrollLockMethod](document.body);
   };
-
+  closeMenuLink.forEach(item => item.addEventListener('click', toggleMenu));
   openMenuBtn.addEventListener('click', toggleMenu);
   closeMenuBtn.addEventListener('click', toggleMenu);
 
@@ -23,3 +24,17 @@
     bodyScrollLock.enableBodyScroll(document.body);
   });
 })();
+
+// const menuLinks = document.querySelectorAll('.mobile - menu__link[data-goto');
+// if (menuLinks > 0) {
+//   menuLinks.forEach(menuLink => {
+//     menuLink.addEventListener('click', onMenuLinkClick);
+//   });
+//   function onMenuLinkClick(e) {
+//     const menuLink = e.target;
+//     e.preventDefault();
+//     if (mobileMenu.classList.toggle('is-open')) {
+//       mobileMenu.classList.remove('is-open');
+//     }
+//   }
+// }
